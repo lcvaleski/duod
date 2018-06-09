381ca6e7df4cb1b7dc8be41a44d98d1cb0f7e7ba
Logan Valeski <loganvaleski@gmail.com>
2018-06-09 14:18:35 +0200

    added voting restrictions to market close windows

--- duod/app/main/testing.py
+++ duod/app/main/testing.py
@@ -1,5 +1,6 @@
 from __future__ import division
 from flask import Flask, render_template, make_response, request
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String
 from flask_bootstrap import Bootstrap
@@ -48,16 +49,20 @@ def home():
 @app.route('/poll/<vote>')
 def handle_vote(vote):
 
-    if not 'vote' in request.cookies:
-        v = Votes(vote)
-        db.session.add(v)
-        db.session.commit()
-        percent_up = int((Votes.query.filter_by(vote='up').count() / Votes.query.count()) * 100)
-        response = make_response( render_template('poll.html', percent_up=percent_up, vote=vote))
+    if not 'vote' in request.cookies: # if the user has not voted, then add their "up" url to DB and return
+        now = datetime.utcnow()
+        if (now.hour == 14 and now.minute >= 30) or (now.hour > 14 and now.hour < 21): # market is not open
+            v = Votes(vote)
+            db.session.add(v)
+            db.session.commit()
+            percent_up = int((Votes.query.filter_by(vote='up').count() / Votes.query.count()) * 100)
+            response = make_response( render_template('poll.html', percent_up=percent_up, vote=vote))
 
-        response.set_cookie('vote', vote)
-        return response
-    else:
+            response.set_cookie('vote', vote)
+            return response
+        else: #user has not voted, but the window is closed
+            return "you cannot vote right now"
+    else: #if the user has voted, then display the poll html without adding to db
         percent_up = int((Votes.query.filter_by(vote='up').count() / Votes.query.count()) * 100)
         response = make_response( render_template('poll.html', percent_up=percent_up, vote=vote))
         return response
