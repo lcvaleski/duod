210f9e9ea04a46556a576d19f893ca231d1298c5
Logan Valeski <loganvaleski@gmail.com>
2018-06-12 13:48:58 +0200

    renamed the main fail. added tests for market closed logic

--- duod/app/main/duod.py
+++ duod/app/main/duod.py
@@ -35,23 +35,71 @@ class Votes(db.Model):
 ################################################################################################################################################################
 ################################################################################################################################################################
 
-@app.route('/')
+@app.route('/') # HOME
 def home():
 
-    if 'vote' in request.cookies:
+    if 'vote' in request.cookies: #if the user has voted, return poll html
         percent_up = int((Votes.query.filter_by(vote='up').count() / Votes.query.count()) * 100)
         response = make_response( render_template('poll.html', percent_up=percent_up, vote=request.cookies.get('vote')))
         return response
 
-    return render_template ('home.html')
+    return render_template ('home.html') # if the above if does not run, render home
+
+def market_closed(cur_time):
+    if ((cur_time.hour == 14 and cur_time.minute >= 30)
+        or
+       (cur_time.hour > 14 and cur_time.hour < 21)):
+       return False
+    return True
+
+def test_market_closed():
+
+    err_message = "ERROR: market_closed not working - "
+    tests_passed = True
+
+    # test some known open and closed time windows
+    closed_time_1 = datetime.strptime("12/06/18 14:05", "%d/%m/%y %H:%M")
+    open_time_1 = datetime.strptime("12/06/18 14:35", "%d/%m/%y %H:%M")
+    closed_time_2 = datetime.strptime("12/06/18 23:05", "%d/%m/%y %H:%M")
+    open_time_2 = datetime.strptime("12/06/18 18:00", "%d/%m/%y %H:%M")
+
+    if not market_closed(closed_time_1):
+        tests_passed = False
+        print err_message + "closed_time_1"
+
+    if market_closed(open_time_1):
+        tests_passed = False
+        print err_message + "open_time_1"
+
+    if not market_closed(closed_time_2):
+        tests_passed = False
+        print err_message + "closed_time_2"
+
+    if market_closed(open_time_2):
+        tests_passed = False
+        print err_message + "open_time_2"
+
+    # TODO test some weekend days
+
+    if tests_passed:
+        print "market_closed() works! all tests_passed"
+
+    return
 
 
-@app.route('/poll/<vote>')
+def market_closed(cur_time):
+    if ((cur_time.hour == 14 and cur_time.minute >= 30)
+        or
+       (cur_time.hour > 14 and cur_time.hour < 21)):
+       return False
+    return True
+
+
+@app.route('/poll/<vote>') # VOTE
 def handle_vote(vote):
 
-    if not 'vote' in request.cookies: # if the user has not voted, then add their "up" url to DB and return
-        now = datetime.utcnow()
-        if (now.hour == 14 and now.minute >= 30) or (now.hour > 14 and now.hour < 21): # market is not open
+    if not 'vote' in request.cookies: # if the user has not voted, add their vote
+        if market_closed(datetime.utcnow()): # market is closed
             v = Votes(vote)
             db.session.add(v)
             db.session.commit()
@@ -60,9 +108,11 @@ def handle_vote(vote):
 
             response.set_cookie('vote', vote)
             return response
-        else: #user has not voted, but the window is closed
-            return "you cannot vote right now"
-    else: #if the user has voted, then display the poll html without adding to db
+        else: # user has not voted, but the window is closed
+            percent_up = int((Votes.query.filter_by(vote='up').count() / Votes.query.count()) * 100)
+            return 'The market is closed boy'
+
+    else: # if the user has voted, then display the poll html without adding to db
         percent_up = int((Votes.query.filter_by(vote='up').count() / Votes.query.count()) * 100)
         response = make_response( render_template('poll.html', percent_up=percent_up, vote=vote))
         return response
