60838b2271660419937fb562945bd6041d90eed8
Logan Valeski <loganvaleski@gmail.com>
2018-06-22 13:59:39 -0600

    Added full vote counter display functionality which includes variables in duod and modifications to html in home.html and poll.html

--- duod/app/main/duod.py
+++ duod/app/main/duod.py
@@ -38,6 +38,9 @@ class Votes(db.Model):
 def percent_up():
     return int((Votes.query.filter_by(vote='up').count() / Votes.query.count()) * 100)
 
+def vote_count(vote_type):
+    return int(Votes.query.filter_by(vote=vote_type).count())
+
 def market_closed(cur_time):
     if (cur_time.weekday() < 5 and
                 ((cur_time.hour == 14 and cur_time.minute >= 30)
@@ -85,29 +88,38 @@ def test_market_closed():
 
 @app.route('/') # HOME
 def home():
+    vote_count_up=vote_count('up')
+    vote_count_down=vote_count('down')
+    p_up=percent_up()
     if 'vote' in request.cookies: #if the user has voted, return poll html
-        response = make_response( render_template('poll.html', percent_up=percent_up(), vote=request.cookies.get('vote')))
-        return response
+        return make_response( render_template('poll.html', percent_up=p_up, vote_count_up=vote_count_up,
+            vote_count_down=vote_count_down, vote=request.cookies.get('vote')))
 
-    return render_template ('home.html', market_closed=market_closed(datetime.utcnow()), percent_up=percent_up()) # if the above if does not run, render home
+    return render_template ('home.html', market_closed=market_closed(datetime.utcnow()), percent_up=p_up,
+        vote_count_up=vote_count_up, vote_count_down=vote_count_down) # if the above if does not run, render home
 
 
 @app.route('/poll/<vote>') # VOTE
 def handle_vote(vote):
-
+    vote_count_up=vote_count('up')
+    vote_count_down=vote_count('down')
+    p_up=percent_up()
     if not 'vote' in request.cookies: # if the user has not voted, add their vote
         if market_closed(datetime.utcnow()): # market is closed and its a weekday
             v = Votes(vote)
             db.session.add(v)
             db.session.commit()
-            response = make_response( render_template('poll.html', percent_up=percent_up(), vote=vote))
+            response = make_response( render_template('poll.html', percent_up=percent_up(),
+                vote_count_up=vote_count('up'), vote_count_down=vote_count('down'), vote=vote))
             response.set_cookie('vote', vote)
             return response
         else: # user has not voted, but the window is closed
-            return render_template("home.html", percent_up=percent_up())
+            return render_template("home.html", percent_up=p_up,
+                vote_count_up=vote_count_up, vote_count_down=vote_count_down)
 
     else: # if the user has voted, then display the poll html without adding to db
-        response = make_response( render_template('poll.html', percent_up=percent_up, vote=vote))
+        response = make_response( render_template('poll.html', percent_up=p_up,
+            vote_count_up=vote_count_up, vote_count_down=vote_count_down, vote=vote))
         return response
 
 ################################################################################################################################################################
